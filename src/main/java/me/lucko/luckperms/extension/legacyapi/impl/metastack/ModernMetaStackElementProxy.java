533264d20b33ad684e463a0a64be53025d4167d4
Luck <git@lucko.me>
2020-05-24 22:28:49 +0100

    Fix NPE in ModernMetaStackElementProxy (lucko/LuckPerms#2327)

--- src/main/java/me/lucko/luckperms/extension/legacyapi/impl/metastack/ModernMetaStackElementProxy.java
+++ src/main/java/me/lucko/luckperms/extension/legacyapi/impl/metastack/ModernMetaStackElementProxy.java
@@ -1,13 +1,18 @@
 package me.lucko.luckperms.extension.legacyapi.impl.metastack;
 
 import com.google.common.collect.Maps;
+
 import me.lucko.luckperms.extension.legacyapi.impl.node.ChatMetaTypeProxyUtil;
 import me.lucko.luckperms.extension.legacyapi.impl.node.NodeProxy;
+
 import net.luckperms.api.metastacking.MetaStackElement;
 import net.luckperms.api.node.ChatMetaType;
 import net.luckperms.api.node.types.ChatMetaNode;
+
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Map;
 
 public class ModernMetaStackElementProxy implements MetaStackElement {
     private final me.lucko.luckperms.api.metastacking.MetaStackElement element;
@@ -18,7 +23,8 @@ public class ModernMetaStackElementProxy implements MetaStackElement {
 
     @Override
     public boolean shouldAccumulate(@NonNull ChatMetaType type, @NonNull ChatMetaNode<?, ?> node, @Nullable ChatMetaNode<?, ?> current) {
-        return this.element.shouldAccumulate(new NodeProxy(node), ChatMetaTypeProxyUtil.legacyType(type), Maps.immutableEntry(current.getPriority(), current.getMetaValue()));
+        Map.Entry<Integer, String> currentEntry = current == null ? null : Maps.immutableEntry(current.getPriority(), current.getMetaValue());
+        return this.element.shouldAccumulate(new NodeProxy(node), ChatMetaTypeProxyUtil.legacyType(type), currentEntry);
     }
 
 }
